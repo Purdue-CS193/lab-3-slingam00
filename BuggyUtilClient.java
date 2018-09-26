688a61d5797e47fe8d09ab13c0c3f29ac201d43e
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2018-09-26 12:52:41 -0400

    Add files via upload

---
+++ BuggyUtilClient.java
@@ -0,0 +1,14 @@
+
+public class BuggyUtilClient {
+    public static void main(String[] args) {
+        // First test case in Main
+        System.out.println("Example 1: Make My Username");
+        System.out.println("In: first = " + "\"Sanjeev\"" +
+                ", last = " + "\"Lingam-Nattamai\"");
+        System.out.println("Out: " +
+        StringUtils.makePurdueUsername("Sanjeev", "Lingam-Nattamai"));
+    }
+}
+
+
+
